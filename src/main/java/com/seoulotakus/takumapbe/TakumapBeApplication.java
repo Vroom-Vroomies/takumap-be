915ecccb5ffee549ca5cfd18901dc5b39a092e31
ddzeun <ddzeun@gmail.com>
2025-10-15 10:59:16 +0900

    feat: 프로젝트 기본 설정

---
+++ src/main/java/com/seoulotakus/takumapbe/TakumapBeApplication.java
@@ -0,0 +1,13 @@
+package com.seoulotakus.takumapbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakumapBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakumapBeApplication.class, args);
+    }
+
+}
